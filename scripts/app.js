eeedc3c2b9a49837680433c74b905b2fd1b4ef7b
Mike Wilcox <mwilcox56@gmail.com>
2015-02-23 21:19:06 -0500

    entry point

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,26 @@
 import React from 'react';
+import Router from 'react-router';
+import Home from './components/dashboard.react';
 
-// TODO: UI
+let { DefaultRoute, Route, RouteHandler } = Router;
+
+let App = React.createClass({
+
+  render() {
+    return (
+      <div className="container">
+
+        <RouteHandler/>
+      </div>
+    );
+  }
+
+});
+
+let routes = <Route handler={App}>
+  <DefaultRoute name="home" handler={Home}/>
+</Route>
+
+Router.run(routes, function(Handler) {
+  React.render(<Handler/>, document.getElementById("app"))
+})
