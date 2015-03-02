4a43bedd5941bd8d9fa71e76a0455abea940b4aa
Mike Wilcox <mwilcox56@gmail.com>
2015-03-01 22:13:37 -0500

    update constants

--- scripts/constants/constants.js
+++ scripts/constants/constants.js
@@ -8,9 +8,21 @@ class Constants {
       USER_LOGIN: null,
       USER_LOGOUT: null,
 
+      DRAFT_ROOM_CREATED: null,
+      DRAFT_STATUS: null,
+      DRAFT_COMPLETE: null,
+      PLAYER_SELECTED: null,
+      START_DRAFT: null,
+      PAUSE_DRAFT: null,
+      STOP_DRAFT: null,
+      CANCEL_DRAFT: null,
+      MEMBER_JOINED: null,
+      MEMBER_LEFT: null,
+
       RECEIVE_PLAYER_STATS: null,
       RECEIVE_PLAYERS_DATA: null,
-      RECEIVE_USER_LEAGUE_INFO: null,
+      RECEIVE_LEAGUES_USER_IS_IN: null,
+      RECEIVE_ALL_LEAGUES: null,
 
       CREATE_LEAGUE: null,
       ADD_NEW_MEMBER: null,
