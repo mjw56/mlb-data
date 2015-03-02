bc29602574057da58ef9c39cc9323a6e993d62ef
Mike Wilcox <mwilcox56@gmail.com>
2015-03-01 22:14:10 -0500

    update app

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import Router from 'react-router';
-import Dashboard from './components/dashboard.react';
+import Dashboard from './components/dashboard/main.react';
 import Authentication from './utils/authentication';
-import Login from './components/login.react';
-import DraftRoom from './components/draft-room.react';
+import Login from './components/login/main.react';
+import DraftRoom from './components/draft-room/main.react';
+import LeagueStore from './stores/league-store';
 
 let { DefaultRoute, Route, RouteHandler, Link } = Router;
 
 let App = React.createClass({
+
+  mixins: [Router.Navigation],
+
+  /*
+  * The main app component can listen for
+  * certain changes from stores to trigger
+  * route changes
+  *
+  * TODO: move to a mixin?
+  */
+  componentDidMount() {
+      LeagueStore.addNewLeagueListener(this._newLeagueAdded);
+  },
+
+  _newLeagueAdded() {
+    let newLeague = LeagueStore.getNewLeague();
+
+    this.transitionTo('draft-room', {name: newLeague.info.name});
+  },
 
   render() {
     return (
@@ -27,9 +47,9 @@ let App = React.createClass({
 let routes = <Route handler={App}>
   <Route name="login" handler={Login}/>
   <Route name="dashboard" handler={Dashboard}/>
-  <Route name="draft-room" handler={DraftRoom}/>
+  <Route name="draft-room" path="/draft-room/:name" handler={DraftRoom}/>
 </Route>
 
-Router.run(routes, function(Handler) {
+Router.run(routes, function(Handler, state) {
   React.render(<Handler/>, document.getElementById("app"))
-})
+});
