2e6d6f551e41408eff1e1760aaad5238c4105c1f
Mike Wilcox <mwilcox56@gmail.com>
2015-03-08 14:13:19 -0400

    re-factor login component

--- scripts/components/login/main.react.js
+++ scripts/components/login/main.react.js
@@ -1,37 +1,15 @@
 import React from 'react/addons';
 import Router from 'react-router';
-import Firebase from 'firebase';
 import UserActions from '../../actions/user-actions';
-import UserStore from '../../stores/user-store';
 
 let PureRenderMixin = React.addons.PureRenderMixin;
 
 export default React.createClass({
 
-  mixins: [ Router.Navigation, PureRenderMixin ],
-
-  statics: {
-    attemptedTransition: null
-  },
-
-  componentDidMount() {
-    UserStore.addChangeListener(this._onChange);
-  },
-
-  _getStateFromStore() {
-    let user = UserStore.getUserInfo();
-
-    if(user.loggedIn) {
-      this.replaceWith('/dashboard');
-    }
-  },
+  mixins: [ PureRenderMixin ],
 
   _login() {
     UserActions.userLogin();
-  },
-
-  _onChange() {
-    this._getStateFromStore();
   },
 
   render() {
